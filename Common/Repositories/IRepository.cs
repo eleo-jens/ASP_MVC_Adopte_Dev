3aa87f0cff57b2dab6f41c60046fe84267c1075d
Eleonore Stultjens <eleonore.jens@gmail.com>
2023-01-25 16:32:24 +0100

    Add project files.

---
+++ Common/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repositories
+{
+    public interface IRepository<TEntity, TId>: IGetRepository<TEntity, TId>, IInsertRepository<TEntity, TId>, TDeleteRepository<TEntity, TId>, IUpdateRepository<TEntity, TId>
+    {
+    }
+}
