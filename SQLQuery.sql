e0f9cc376ac9c7482d9ed74c1374c500c95ab63c
Eleonore <eleonore.jens@gmail.com>
2023-02-06 21:15:30 +0100

    SQL GetCategoriesByDeveloper(id)

---
+++ SQLQuery.sql
@@ -0,0 +1,6 @@
+﻿SELECT DISTINCT Categories.CategLabel, Categories.idCategory, LangCateg.idIT from LangCateg
+JOIN DevLang
+ON Devlang.idIT = LangCateg.idIT
+JOIN Categories
+ON Categories.idCategory = LangCateg.idCategory
+WHERE idDev = 2
